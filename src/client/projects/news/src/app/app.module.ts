021a9915843ce1663403e9f5e082b0b2406bff13
tonysatrem <flewdash777@gmail.com>
2024-03-25 16:28:12 +0300

    refactor; news components

--- src/client/projects/news/src/app/app.module.ts
+++ src/client/projects/news/src/app/app.module.ts
@@ -2,11 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppRootComponent } from './components/app-root/app.component';
+import { HeaderComponent } from './components/header-component/header.component';
+import { NewsComponent } from './components/news-component/news.component';
+import { NewsBlockComponent } from './components/news-block-component/news-block.component';
+import { FoodbarComponent } from './components/foodbar-component/foodbar.component';
 
 @NgModule({
   declarations: [
-    AppRootComponent
+    AppRootComponent,
+    HeaderComponent,
+    NewsComponent,
+    NewsBlockComponent,
+    FoodbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/client/projects/news/src/app/components/news-block-component/news-block.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-news-block',
+  templateUrl: './news-block.component.html',
+  styleUrls: ['./news-block.component.scss']
+})
+export class NewsBlockComponent {
+
+}
