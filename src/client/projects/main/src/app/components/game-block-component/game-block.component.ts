5abef4f0093dd090e5c194913a2d7736f57c7b7a
tonysatrem <flewdash777@gmail.com>
2024-04-01 22:03:17 +0300

    fix sliders

--- src/client/projects/main/src/app/components/game-block-component/game-block.component.ts
+++ src/client/projects/main/src/app/components/game-block-component/game-block.component.ts
@@ -1,20 +1,11 @@
-import { DOCUMENT } from '@angular/common';
-import { AfterViewInit, Component, ElementRef, Inject, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { IGameBlock } from '../../models/IGameBlock';
-import slider from 'main/src/utils/slider';
-
 
 @Component({
   selector: 'app-game-block',
   templateUrl: './game-block.component.html',
   styleUrls: ['./game-block.component.scss']
 })
-export class GameBlockComponent implements AfterViewInit {
+export class GameBlockComponent {
   @Input() game: IGameBlock
-
-  constructor(@Inject(DOCUMENT) private document: Document, private elementRef: ElementRef) {}
-  
-  ngAfterViewInit() {
-    slider()
-  }
 }
